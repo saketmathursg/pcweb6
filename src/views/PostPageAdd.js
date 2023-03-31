f8998c1b7e8d6b40e0230e65e7204ed5858bfe86
saket mathur <saket.mathur@icloud.com>
2023-03-31 20:32:12 +0800

    included option to upload photo for add page

--- src/views/PostPageAdd.js
+++ src/views/PostPageAdd.js
@@ -3,8 +3,9 @@ import { Button, Container, Form, Nav, Navbar } from "react-bootstrap";
 import { addDoc, collection } from "firebase/firestore";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
-import { auth, db } from "../firebase";
+import { auth, db, storage } from "../firebase";
 import { signOut } from "firebase/auth";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 
 export default function PostPageAdd() {
@@ -15,7 +16,10 @@ export default function PostPageAdd() {
 
 
   async function addPost() {
-    await addDoc(collection(db, "posts"), { caption, image });
+    const imageReference = ref(storage, `images/${image.name}`);
+    const response = await uploadBytes(imageReference, image);
+    const imageUrl = await getDownloadURL(response.ref);
+    await addDoc(collection(db, "posts"), { caption, image: imageUrl });
     navigate("/");
   }
 
@@ -50,16 +54,11 @@ export default function PostPageAdd() {
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="image">
-            <Form.Label>Image URL</Form.Label>
+            <Form.Label>Image</Form.Label>
             <Form.Control
-              type="text"
-              placeholder="https://zca.sg/img/1"
-              value={image}
-              onChange={(text) => setImage(text.target.value)}
+              type="file"
+              onChange={(e) => setImage(e.target.files[0])}
             />
-            <Form.Text className="text-muted">
-              Make sure the url has a image type at the end: jpg, jpeg, png.
-            </Form.Text>
           </Form.Group>
           <Button variant="primary" onClick={async (e) => addPost()}>
             Submit
